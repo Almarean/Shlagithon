6918fd33ff87d43a3e32d6fd804cf4e950c0eb30
ThomasL <thomaslaure3@gmail.com>
2020-03-11 09:20:20 +0100

    CSS Bootstrap

--- Views/templates/head.php
+++ Views/templates/head.php
@@ -20,6 +20,5 @@
 <link rel="shortcut icon" href="">
 <link rel="apple-touch-icon" href="">
 <meta name="msapplication-square310x310logo" content="">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 <link rel="stylesheet" href="../assets/css/style.css">

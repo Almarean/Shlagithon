8b02e0c2175c8344912813c6e9601cf9c8b66fce
ThomasL <thomaslaure3@gmail.com>
2020-03-17 15:23:36 +0100

    Amélioration structure code

--- Controllers/AdminShowMembersController.php
+++ Controllers/AdminShowMembersController.php
@@ -3,11 +3,12 @@
 namespace App\Controllers;
 
 use App\Models\Member;
-use App\Services;
-$members = Services\MemberManager::findAllByDateCreation();
-if(isset($_GET['deleteLogin'])){
-    Services\MemberManager::deleteOneById($_GET['deleteLogin']);
-    header('location:members-editor');
+use App\Services\MemberManager;
+
+$members = MemberManager::findAllByDateCreation();
+if (isset($_GET['deleteLogin'])) {
+    MemberManager::deleteOneById($_GET['deleteLogin']);
+    header('Location: members-editor');
 }
 
 include __DIR__ . "/../Views/admin_show_members.php";
--- Views/admin_show_members.php
+++ Views/admin_show_members.php
@@ -4,56 +4,57 @@
 <head>
     <?php include __DIR__ . "/templates/head.php"; ?>
 </head>
+
 <body>
-<?php include __DIR__ . "/templates/header_admin.php"; ?>
+    <?php include __DIR__ . "/templates/header_admin.php"; ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12" id="member-tab">
-            <div class="main-box clearfix">
-                <div class="table-responsive">
-                    <table class="table user-list">
-                        <thead>
-                        <tr>
-                            <th><span>Status</span></th>
-                            <th><span>Identité</span></th>
-                            <th><span>Date de création</span></th>
-                            <th><span>Dernière connexion</span></th>
-                            <th><span>Email</span></th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($members as $member){ ?>
-                            <tr>
-                            <td>
-                                <span class="user-subhead"><?php echo ($member->getIsConfirmed())?'Admin':'Membre';?> </span>
-                            </td>
-                            <td>
-                                <a href="#" class="user-link"><?php echo $member->getName() . ' '.$member->getFirstname();?> </a>
-                            </td>
-                            <td>
-                                <?php echo $member->getCreationDate(); ?>
-                            </td>
-                            <td>
-                                <?php echo $member->getLastConnectionDate(); ?>
-                            </td>
-                            <td>
-                                <a href="#"><?php echo $member->getEmail(); ?></a>
-                            </td>
-                            <td style="width: 30%;">
-                                <div class="mx-2 d-inline"><a href="#" class="table-link" ><i class="fas fa-pencil-alt"></i></a></div>
-                                <div class="mx-2 d-inline"><a href="?deleteLogin=<?php echo $member->getId();?>" class="table-link" ><i class="fas fa-trash" style="color: red"></i></a></div>
-                            </td>
-                        </tr>
-                        <?php } ?>
-
-
-
-<?php include __DIR__ . "/templates/footer.php"; ?>
-
-
-
-<?php include __DIR__ . "/templates/scriptsjs.php"; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12" id="member-tab">
+                <div class="main-box clearfix">
+                    <div class="table-responsive">
+                        <table class="table user-list">
+                            <thead>
+                                <tr>
+                                    <th><span>Status</span></th>
+                                    <th><span>Identité</span></th>
+                                    <th><span>Date de création</span></th>
+                                    <th><span>Dernière connexion</span></th>
+                                    <th><span>Email</span></th>
+                                    <th><span>Actions</span></th>
+                                </tr>
+                            </thead>
+                            <?php foreach ($members as $member) { ?>
+                                <tr>
+                                    <td>
+                                        <span class="user-subhead"><?php echo ($member->getIsConfirmed()) ? 'Admin' : 'Membre'; ?> </span>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="user-link"><?php echo $member->getName() . ' ' . $member->getFirstname(); ?> </a>
+                                    </td>
+                                    <td>
+                                        <?php echo $member->getCreationDate(); ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $member->getLastConnectionDate(); ?>
+                                    </td>
+                                    <td>
+                                        <a href="#"><?php echo $member->getEmail(); ?></a>
+                                    </td>
+                                    <td style="width: 30%;">
+                                        <div class="mx-2 d-inline"><a href="#" class="table-link"><i class="fas fa-pencil-alt"></i></a></div>
+                                        <div class="mx-2 d-inline"><a href="?deleteLogin=<?php echo $member->getId(); ?>" class="table-link"><i class="fas fa-trash" style="color: red"></i></a></div>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include __DIR__ . "/templates/footer.php"; ?>
+    <?php include __DIR__ . "/templates/scriptsjs.php"; ?>
 </body>
+
 </html>
